f3d62a516a08ad8d5b6d8220daa4d1f19b9ce7a7
Nems <nemesio@adlinks.io>
2023-01-07 23:22:10 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,52 @@
+import mysql.connector
+
+conn = mysql.connector.connect(user='root', password='my-secret-pw', host='127.0.0.1')
+cursor = conn.cursor()
+
+def create_database(dbname):
+    cursor.execute("DROP database IF EXISTS {}".format(dbname))
+    sql = "CREATE database {}".format(dbname)
+    cursor.execute(sql)
+    conn.close()
+    return cursor
+
+def show_database():
+    print("List of databases: ")
+    cursor.execute("SHOW DATABASES")
+    print(cursor.fetchall())
+    conn.close()
+
+def create_table(table,schema):
+    cursor.execute("DROP table IF EXISTS {}".format(table))
+    cursor.execute("CREATE TABLE {} ({})".format(table,schema))
+    conn.close()
+
+def show_table():
+    cursor.execute("SHOW TABLES")
+    print(cursor.fetchall())
+    conn.close()
+
+def query_table(table):
+    cursor.execute("SELECT * FROM {}".format(table))
+    print(cursor.fetchall())
+    conn.close()
+
+def insert_table(table, values):
+
+def update_table(table, value):
+
+def delete_table(table):
+
+if __name__ == '__main__':
+    testdb = "testdatabase"
+    table = "test"
+    schema = "name VARCHAR(255), address VARCHAR(255)"
+    create_database(testdb)
+    crete_table(table,schema)
+    show_database()
+
+
+
+
+
+
